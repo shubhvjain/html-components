8ff68fe325fa7358d30ad24120b816a305ac6d37
shubh <shubhvjain@gmail.com>
2024-10-20 19:40:59 +0200

    new ver with exports

--- src/lib/index.js
+++ src/lib/index.js
@@ -1,1 +1,23 @@
 // Reexport your entry components here
+// src/index.js
+// Export each component individually as named exports
+
+
+export {default as Workspace} from "./WorkSpace.svelte"
+
+export {default as Record} from "./beanbagdb/Record.svelte"
+export {default as RecordSearch} from "./beanbagdb/RecordSearch.svelte"
+export {default as Schema} from "./beanbagdb/Schema.svelte"
+
+export {default as Block} from "./blockpage/Block.svelte"
+export {default as Page} from "./blockpage/Page.svelte"
+
+export {default as HelpDoc} from "./helppage/HelpDoc.svelte"
+
+export {default as Content} from "./learningbox/Content.svelte"
+export {default as LearnBoard} from "./learningbox/LearnBoard.svelte"
+export {default as PracticeSession} from "./learningbox/PracticeSession.svelte"
+export {default as QuestionsQueue} from "./learningbox/QuestionsQueue.svelte"
+export {default as ReviewSessions} from "./learningbox/ReviewSessions.svelte"
+export {default as SubjectNetwork} from "./learningbox/SubjectNetwork.svelte"
+
